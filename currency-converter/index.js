84248f9434cfe6c298a97683222604bbefbe7450
Adeel_Ahmed <155395902+AdeelAhmed03@users.noreply.github.com>
2024-05-22 23:36:28 +0500

    Add files via upload

---
+++ currency-converter/index.js
@@ -0,0 +1,36 @@
+import inquirer from "inquirer";
+const currency = {
+    USD: 1, // Base currency
+    EUR: 0.91,
+    GBP: 0.76,
+    INR: 74.57,
+    PKR: 280
+};
+const answers = await inquirer.prompt([
+    {
+        name: "from",
+        message: "Enter From Currency",
+        type: 'list',
+        choices: ['USD', 'EUR', 'GBP', 'INR', 'PKR'],
+    },
+    {
+        name: "to",
+        message: "Enter To Currency",
+        type: 'list',
+        choices: ['USD', 'EUR', 'GBP', 'INR', 'PKR'],
+    },
+    {
+        name: 'amount',
+        message: 'Enter Your Amount',
+        type: 'number'
+    },
+]);
+let fromCurrency = currency[answers.from]; // exchange rate
+let toCurrency = currency[answers.to]; // exchange rate
+let amount = answers.amount;
+let baseAmount = amount / fromCurrency; // USD base currency
+let convertedAmount = baseAmount * toCurrency;
+console.log(Math.floor(Math.ceil(convertedAmount)));
+// console.log(fromCurrency);
+// console.log(toCurrency);
+// console.log(amount);
